1cb78ca554472456fae93ef00714f3ee0a4338af
BeenYan <asdfghjkl42045@gmail.com>
2023-02-15 19:35:01 +0800

    try build again

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+export TAURI_PRIVATE_KEY=`cat ~/.tauri/tauri_updater.key`
+export TAURI_KEY_PASSWORD=""
 pnpm tauri build
--- setenv.sh
+++
@@ -1,3 +0,0 @@
-# 執行指令 source setenv.sh
-export TAURI_PRIVATE_KEY=`cat ~/.tauri/tauri_updater.key`
-export TAURI_KEY_PASSWORD=""
